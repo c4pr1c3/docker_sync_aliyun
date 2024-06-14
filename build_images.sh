6db886cc7cb97ac8b5d96b775d772f3da0e06f23
huangwei <huangwei.me@cuc.edu.cn>
2024-06-14 15:47:14 +0800

    fix images.yaml output format error

--- build_images.sh
+++ build_images.sh
@@ -11,8 +11,10 @@ fi
 # 从 images.txt 读取镜像名称和版本
 while IFS= read -r line; do
     img_name=$(echo $line | cut -d ':' -f 1)
+    # 如果 img_name 包含 / 需要替换为 -
+    mirror_img_name=$(echo $img_name | sed 's/\//\-/g')
     img_tag=$(echo $line | cut -d ':' -f 2)
-    conf_line="${img_name}:${img_tag}: $BASE_HOST/${ACR_NAMESPACE}/${img_name}:${img_tag}"
+    conf_line="${img_name}:${img_tag}: $BASE_HOST/${ACR_NAMESPACE}/${mirror_img_name}:${img_tag}"
     echo "$conf_line" >> images.yaml
 done < "images.txt"
 
